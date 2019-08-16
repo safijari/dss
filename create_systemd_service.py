fe1a636a97fbb0f5a8c0f98f419df82bb8ab04d0
Jariullah Safi <safijari@isu.edu>
2019-08-16 11:11:37 -0700

    Adding first bullshit script

---
+++ create_systemd_service.py
@@ -0,0 +1,50 @@
+from bash import bash
+import os
+
+base_str="""[Unit]
+Description={description}
+
+[Service]
+Type={service_type}
+User={service_user}
+ExecStart={start_command}
+# Restart=on-failure
+{restart_block}
+# Other restart options: always, on-abort, etc
+
+[Install]
+WantedBy={target}.target"""
+
+
+def make_systemd_service(name, description, service_user, service_type='simple', restart_on_fail=True, wanted_by='multi-user'):
+    if restart_on_fail:
+        restart_block = "Restart=on-failure"
+
+    service_str = base_str.format(**locals())
+
+    path = f"/etc/systemd/system/{name}.service"
+
+    assert not os.path.exists(path), f"Service at {path} already exists, won't overwrite."
+
+    with open(path, 'w') as ff:
+        ff.write(service_str)
+
+    print("enabling service")
+    enable_cmd = bash(f'sudo systemctl enable {name}')
+
+    print(enable_cmd.stdout)
+    print(enable_cmd.stderr)
+
+    print("starting service")
+
+    enable_cmd = bash(f'sudo systemctl start {name}')
+
+    print(enable_cmd.stdout)
+    print(enable_cmd.stderr)
+
+    print("statusing service")
+
+    enable_cmd = bash(f'sudo systemctl status {name}')
+
+    print(enable_cmd.stdout)
+    print(enable_cmd.stderr)
